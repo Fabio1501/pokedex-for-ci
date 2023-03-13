0ffd24a5ede95af9426708fb2332b4ce01d573d3
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-03-13 11:28:18 -0300

    e

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,8 @@
-module.exports = (on, config) => {
-  config.videoUploadOnPasses = false
-  return config
-}
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  component: {
+    baseUrl : "http://localhost:5000"
+  },
+  videoUploadOnPasses : false
+})
